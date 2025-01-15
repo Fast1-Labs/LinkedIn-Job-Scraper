5a43305ccc1105003275d8b581cd965660a0ff56
anilyavas <anil.yavas99@gmail.com>
2025-01-15 15:16:53 +0300

    app initialize

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,7 +6,7 @@ import jobRoutes from './routes/jobRoutes';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
